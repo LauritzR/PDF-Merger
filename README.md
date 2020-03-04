20eac699dbd19631bdae602b9b6550c8a9846f3a
Lauritz Rasbach <rasbachlauritz@googlemail.com>
2020-03-04 12:21:32 +0100

    addes descending order and order by time fixes #1

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ A python script for concatenating PDFs. Can be used for e-books, lecture slides 
 
 ## How to use
 Put all your PDF files in one directory. <br>
-Then simply run `python3 pdf_merger.py /path/to/directory`. <br>
+Then simply run `python3 pdf_merger.py`. <br>
+You can specify the path to a directory using `-p`. If you don't, the current directory is used.<br>
+Use `-t` to sort by time.<br>
+Use `-desc`for descending order <br>
 The output will be given as result.pdf in the same directory as pdf_merger.py.
 
 ! The files should be in alpabetical order (ideally first file "01filename.pdf" and so on) !
--- pdf_merger.py
+++ pdf_merger.py
@@ -1,15 +1,24 @@
 import os
-import sys
-
+import pathlib
+import argparse
 from PyPDF2 import PdfFileMerger
 
-directory = sys.argv[1]
+parser = argparse.ArgumentParser(description='Append PDF files in a directory.')
+parser.add_argument('-p',metavar='--path',help='path to target dirctory, default=current directory', type=str, nargs=1, default=[pathlib.Path(__file__).parent.absolute()])
+parser.add_argument('-t', help='flag for order by time created, default=False', action="store_true", default=False)
+parser.add_argument('-desc', help='flag for descending order, default=False', action="store_true", default=False)
+
+args = parser.parse_args()
+
+directory = args.p[0]
 
 merger = PdfFileMerger()
 
-for e in sorted(os.listdir(directory)):
+order_by = os.path.getmtime if args.t else str
+
+for e in sorted(os.listdir(directory), reverse = args.desc ):
     if e[-3:] == "pdf":
-        merger.append(directory+"/"+e)
+        merger.append(str(directory)+"/"+e)
 
 merger.write("result.pdf")
 merger.close()
